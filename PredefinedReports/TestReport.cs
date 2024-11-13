a07bf872778343013fbd72e3ff2b7a2e78fc9fed
PavanReddyYK <pavanykreddy24@gmail.com>
2024-11-13 16:13:02 +0530

    pushing code to remote branch

---
+++ PredefinedReports/TestReport.cs
@@ -0,0 +1,7 @@
+﻿namespace ExploreProject2.PredefinedReports {
+    public partial class TestReport : DevExpress.XtraReports.UI.XtraReport {
+        public TestReport() {
+            InitializeComponent();
+        }
+    }
+}
